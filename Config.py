ae18c1f4b209ce216da166efa435dfdb7b70f012
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-20 22:15:44 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,39 @@
+from os import path, getenv
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+
+
+class config:
+
+    API_ID = "8623612"
+    API_HASH = "06ea2889c5517eb64017b032d667e29f"
+    BOT_TOKEN = "6396685550:AAFhzt65XsaJYYUD0oG5ktgZVz4UHIQaOtg"
+    SUDO_USERS = 5857041668
+    CHANNELS = is_enabled((os.environ.get("CHANNELS", "True")), True)
+    CHANNEL_ID = (
+        [int(i.strip()) for i in os.environ.get("CHANNEL_ID", "-1002119954783").split(" ")]
+        if os.environ.get("CHANNEL_ID")
+        else []
+    )
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5857041668"))
+    SUDO_USERS.append(OWNER_ID) if OWNER_ID not in SUDO_USERS else []
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Amala203145:Amala2031456@cluster0.t9ibfge.mongodb.net/?retryWrites=true&w=majority")  
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "TgfileStoringBot")
+    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002119954783"))
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', "-1002119954783"))
+    FILE_STORE_CHANNEL = [int(ch) for ch in (os.environ.get('FILE_STORE_CHANNEL', '-1002119954783')).split()]
+    PUBLIC_FILE_STORE = is_enabled(os.environ.get('PUBLIC_FILE_STORE', "True"), True)

4ba4355c2214a4a8c598a09554ac376fa4271946
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-21 16:25:45 +0530

    Update start.py

--- plugins/modules/start.py
+++ plugins/modules/start.py
@@ -1,9 +1,8 @@
-from pyrogram import filters, Client 
+from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from plugins import app 
 from pyrogram.errors.exceptions import UserNotParticipant
 
-force_channel = -1002119954783
+force_channel_id = -1002119954783
 
 start_keyboard = InlineKeyboardMarkup([[
     InlineKeyboardButton("Help", callback_data="help"),
@@ -16,65 +15,54 @@ about_keyboard = InlineKeyboardMarkup([[
 
 help_keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("⇦Back", callback_data="start")]])
 
-@app.on_message(filters.command(["start", "help"]) & filters.private)
+async def check_membership(user_id):
+    try:
+        user = await app.get_chat_member(force_channel_id, user_id)
+        return user.status != "kicked"
+    except UserNotParticipant:
+        return False
+
+@app.on_message(filters.command(["start"]) & filters.private)
 async def start(_, message: Message):
-    if force_channel:
-        try:
-            user = await app.get_chat_member(force_channel, message.from_user.id)
-            if user.status == "kicked":
-                await message.reply_text("You are banned.")
-                return
-        except UserNotParticipant:
+    if force_channel_id:
+        if not await check_membership(message.from_user.id):
             keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton("Join Channel", url=f"https://t.me/TgBotsNetwork")]
+                [InlineKeyboardButton("Join Channel", url=f"https://t.me/TgBotsNetwork")],
+                [InlineKeyboardButton("Try Again", callback_data="again")]
             ])
-            await message.reply_text("You are not joined in my channel. Please join the channel to use me.", reply_markup=keyboard)
+            await message.reply_text(
+                "You are not joined in my channel. Please join the channel to use me.",
+                reply_markup=keyboard
+            )
             return
 
-    if message.command and message.command[0].lower() == "help":
-        await message.reply_text(
-            "Welcome to [TG FILE STORING BOT!](t.me/Tgfilestoringbot) Send any type of media, and I'll generate a special link for you.\n\n"
-            "Commands:\n"
-            "/start - Start using the bot\n"
-            "/help - Display this help message\n"
-            "<u>**Admin commands**</u>\n"
-            "/broadcast - Broadcast a message to all users\n"
-            "/stats - Display bot statistics\n"
-            "/batch - Perform batch operations\n"
-            "/genlink - Generate a special link for a file\n\n"
-            "<u>**Note:**</u> if you want access admin commmands by a premium membership",
-            reply_markup=help_keyboard
-        )
-        return
-
     await message.reply_text(
-        f"Hello {message.from_user.mention}\n\nI am a private files save bot. I can save private files on certain channels, and other users can access them from a special link.",
-        reply_markup=start_keyboard
-    )
-
-    await message.reply_text(
-        f"Hello {message.from_user.mention}\n\nI am a private files save bot. I can save private files on certain channels, and other users can access them from a special link.",
+        f"Hello {message.from_user.mention}\n\nI am a private files save bot. "
+        "I can save private files on certain channels, and other users can access them from a special link.",
         reply_markup=start_keyboard
     )
 
 @app.on_callback_query(filters.regex("about"))
-async def about_callback(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(
-        text="<b><u>About</u></b>\n\n<b>➺Bot Name:</b>[TG FILE STORING BOT](t.me/Tgfilestoringbot)\n<b>➺Language:</b>[python](https://python.org)\n<b>➺Library:</b>[pyrogram](https://pyrogram.org)\n<b>➺Devloped By:</b>[Nobitha](t.me/my_name_is_nobitha)", 
+async def about_callback(_, callback_query):
+    await callback_query.edit_message_text(
+        text="<b><u>About</u></b>\n\n<b>➺Bot Name:</b>[TG FILE STORING BOT](t.me/Tgfilestoringbot)\n"
+             "<b>➺Language:</b>[python](https://python.org)\n<b>➺Library:</b>[pyrogram](https://pyrogram.org)\n"
+             "<b>➺Developed By:</b>[Nobitha](t.me/my_name_is_nobitha)",
         disable_web_page_preview=True,
         reply_markup=about_keyboard,
     )
 
 @app.on_callback_query(filters.regex("start"))
-async def start_callback(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(
-        text=f"Hello {CallbackQuery.message.from_user.mention}\n\nI am a private files save bot. I can save private files on certain channels, and other users can access them from a special link.", 
+async def start_callback(_, callback_query):
+    await callback_query.edit_message_text(
+        text=f"Hello {callback_query.message.from_user.mention}\n\nI am a private files save bot. "
+             "I can save private files on certain channels, and other users can access them from a special link.",
         reply_markup=start_keyboard,
     )
 
 @app.on_callback_query(filters.regex("help"))
-async def help_callback(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(
+async def help_callback(_, callback_query):
+    await callback_query.edit_message_text(
         text="Welcome to [TG FILE STORING BOT!](t.me/Tgfilestoringbot) Send any type of media, and I'll generate a special link for you.\n\n"
              "Commands:\n"
              "/start - Start using the bot\n"
@@ -84,6 +72,33 @@ async def help_callback(client, CallbackQuery):
              "/stats - Display bot statistics\n"
              "/batch - Perform batch operations\n"
              "/genlink - Generate a special link for a file\n\n"
-             "<u>**Note:**</u> if you want access admin commmands by a premium membership",
+             "<u>**Note:**</u> if you want access admin commands by a premium membership",
         reply_markup=help_keyboard
     )
+
+app.on_message(filters.command("help") & filters.private)
+async def help(_, message: Message):
+    await message.reply_text(
+        "Welcome to [TG FILE STORING BOT!](t.me/Tgfilestoringbot) Send any type of media, and I'll generate a special link for you.\n\n"
+        "Commands:\n"
+        "/start - Start using the bot\n"
+        "/help - Display this help message\n"
+        "<u>**Admin commands**</u>\n"
+        "/broadcast - Broadcast a message to all users\n"
+        "/stats - Display bot statistics\n"
+        "/batch - Perform batch operations\n"
+        "/genlink - Generate a special link for a file\n\n"
+        "<u>**Note:**</u> if you want access admin commands by a premium membership",
+        reply_markup=help_keyboard
+    )
+
+@app.on_callback_query(filters.regex("again"))
+async def again_callback(_, callback_query):
+    if await check_membership(callback_query.from_user.id):
+        await callback_query.edit_message_text(
+            text=f"Hello {callback_query.from_user.mention}\n\nYou have successfully joined the channel! "
+                 "Now you can use the bot to save private files on certain channels and access them from a special link.",
+            reply_markup=start_keyboard
+        )
+    else:
+        await callback_query.answer("Please join the channel to use the bot.")

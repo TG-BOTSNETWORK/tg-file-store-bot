7b145c6e73af4cf5177426830f1ebb4f7cf6f963
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-21 14:10:59 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,12 +1,12 @@
-from pyrogram import Client, filters, idle
+rom pyrogram import Client, filters, idle
 from Config import config
 
-with Client(
-    ":tgstore:",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-    plugins=dict(root="plugins.modules")
-) as tgstore:
-    print("Bot started!")
-    tgstore.run()
+tgstore = Client(
+       ":tgstore:",
+       api_id=config.API_ID,
+       api_hash=config.API_HASH,
+       bot_token=config.BOT_TOKEN,
+       plugins=dict(root="plugins.modules")
+)
+print("Bot started!")
+tgstore.start()

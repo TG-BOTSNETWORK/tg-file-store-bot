b66db7fe8c3a3bb8577e94927f3a46c591addae8
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-21 16:08:51 +0530

    Update start.py

--- plugins/modules/start.py
+++ plugins/modules/start.py
@@ -1,4 +1,3 @@
-
 from pyrogram import filters, Client 
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from plugins import app 
@@ -6,18 +5,19 @@ from pyrogram.errors.exceptions import UserNotParticipant
 
 force_channel = -1002119954783
 
-start_keyboard = InlineKeyboardMarkup( [[
-        InlineKeyboardButton("Help", callback_data="about"),
-        InlineKeyboardButton("About", callback_data="about")
-        ]]
-        )
-        
-about_keyboard = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("⇦Back", callback_data="start")
-       ]]
-       )
-              
-@app.on_message(filters.command("start") & filters.private)
+start_keyboard = InlineKeyboardMarkup([[
+    InlineKeyboardButton("Help", callback_data="help"),
+    InlineKeyboardButton("About", callback_data="about")
+]])
+
+about_keyboard = InlineKeyboardMarkup([[
+    InlineKeyboardButton("⇦Back", callback_data="start")
+]])
+
+help_keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("⇦Back", callback_data="start")]])
+
+@admin_check
+@app.on_message(filters.command(["start", "help"]) & filters.private)
 async def start(corn, message: Message):
     if force_channel:
         try:
@@ -32,15 +32,34 @@ async def start(corn, message: Message):
             await message.reply_text("You are not joined in my channel. Please join the channel to use me.", reply_markup=keyboard)
             return
 
-    await message.reply_text(f"Hello {message.from_user.mention}\n\nI am a private files save bot. I can save private files on certain channels, and other users can access them from a special link.", reply_markup=start_keyboard)
+    if message.command and message.command[0].lower() == "help":
+        await message.reply_text(
+            ""Welcome to [TG FILE STORING BOT!](t.me/Tgfilestoringbot) Send any type of media, and I'll generate a special link for you.\n\n"
+             "Commands:\n"
+             "/start - Start using the bot\n"
+             "/help - Display this help message\n"
+             "<u>**Admin commands**</u>\n"
+             "/broadcast - Broadcast a message to all users\n"
+             "/stats - Display bot statistics\n"
+             "/batch - Perform batch operations\n"
+             "/genlink - Generate a special link for a file\n\n"
+             "<u>**Note:**</u> if you want access admin commmands by a premium membership",
+             reply_markup=help_keyboard
+        )
+        return
+
+    await message.reply_text(
+        f"Hello {message.from_user.mention}\n\nI am a private files save bot. I can save private files on certain channels, and other users can access them from a special link.",
+        reply_markup=start_keyboard
+    )
 
 @app.on_callback_query(filters.regex("about"))
 async def about_callback(client, CallbackQuery):
     await CallbackQuery.edit_message_text(
         text="<b><u>About</u></b>\n\n<b>➺Bot Name:</b>[TG FILE STORING BOT](t.me/Tgfilestoringbot)\n<b>➺Language:</b>[python](https://python.org)\n<b>➺Library:</b>[pyrogram](https://pyrogram.org)\n<b>➺Devloped By:</b>[Nobitha](t.me/my_name_is_nobitha)", 
-        disable_web_page_preview=True,    
+        disable_web_page_preview=True,
         reply_markup=about_keyboard,
-    )    
+    )
 
 @app.on_callback_query(filters.regex("start"))
 async def start_callback(client, CallbackQuery):
@@ -48,3 +67,21 @@ async def start_callback(client, CallbackQuery):
         text=f"Hello {CallbackQuery.message.from_user.mention}\n\nI am a private files save bot. I can save private files on certain channels, and other users can access them from a special link.", 
         reply_markup=start_keyboard,
     )
+
+@app.on_callback_query(filters.regex("help"))
+async def help_callback(client, CallbackQuery):
+    await CallbackQuery.edit_message_text(
+        text="Welcome to [TG FILE STORING BOT!](t.me/Tgfilestoringbot) Send any type of media, and I'll generate a special link for you.\n\n"
+             "Commands:\n"
+             "/start - Start using the bot\n"
+             "/help - Display this help message\n"
+             "<u>**Admin commands**</u>\n"
+             "/broadcast - Broadcast a message to all users\n"
+             "/stats - Display bot statistics\n"
+             "/batch - Perform batch operations\n"
+             "/genlink - Generate a special link for a file\n\n"
+             "<u>**Note:**</u> if you want access admin commmands by a premium membership",
+        reply_markup=help_keyboard
+    )
+
+# Add your admin command implementations here

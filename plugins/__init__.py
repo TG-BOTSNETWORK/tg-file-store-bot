b57d493d2201e560af2025c34ce31520bf705c3b
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-21 14:26:31 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from Config import config
 
 tgstore = Client(
-    session_name=":storing:",  # Set a unique session name
+    ":storing:",  
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,

80da5880da0a6473addab49edab11990191637f5
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-21 14:36:33 +0530

    Update start.py

--- plugins/modules/start.py
+++ plugins/modules/start.py
@@ -1,7 +1,7 @@
 
 from pyrogram import filters, Client 
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from plugins import corn 
+from plugins import app 
 from pyrogram.errors.exceptions import UserNotParticipant
 
 force_channel = -1002119954783
@@ -17,11 +17,11 @@ about_keyboard = InlineKeyboardMarkup( [[
        ]]
        )
               
-@tgstore.on_message(filters.command("start") & filters.private)
+@app.on_message(filters.command("start") & filters.private)
 async def start(corn, message: Message):
     if force_channel:
         try:
-            user = await tgstore.get_chat_member(force_channel, message.from_user.id)
+            user = await app.get_chat_member(force_channel, message.from_user.id)
             if user.status == "kicked":
                 await message.reply_text("You are banned.")
                 return
@@ -34,7 +34,7 @@ async def start(corn, message: Message):
 
     await message.reply_text(f"Hello {message.from_user.mention}\n\nI am a private files save bot. I can save private files on certain channels, and other users can access them from a special link.", reply_markup=start_keyboard)
 
-@tgstore.on_callback_query(filters.regex("about"))
+@app.on_callback_query(filters.regex("about"))
 async def about_callback(client, CallbackQuery):
     await CallbackQuery.edit_message_text(
         text="<b><u>About</u></b>\n\n<b>➺Bot Name:</b>[TG FILE STORING BOT](t.me/Tgfilestoringbot)\n<b>➺Language:</b>[python](https://python.org)\n<b>➺Library:</b>[pyrogram](https://pyrogram.org)\n<b>➺Devloped By:</b>[Nobitha](t.me/my_name_is_nobitha)", 
@@ -42,7 +42,7 @@ async def about_callback(client, CallbackQuery):
         reply_markup=about_keyboard,
     )    
 
-@tgstore.on_callback_query(filters.regex("start"))
+@app.on_callback_query(filters.regex("start"))
 async def start_callback(client, CallbackQuery):
     await CallbackQuery.edit_message_text(
         text=f"Hello {CallbackQuery.message.from_user.mention}\n\nI am a private files save bot. I can save private files on certain channels, and other users can access them from a special link.", 

cea6bd1aeb97a121db3c48ab9070a02cfe97d28b
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-21 17:03:50 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,7 +2,7 @@ from pyrogram import Client, idle
 from Config import config
 
 tgbots = Client(
-    ":memory:",  
+    ":santhu:",  
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,

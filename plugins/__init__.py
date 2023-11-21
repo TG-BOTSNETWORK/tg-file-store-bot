bfac649c06b9d4c811d45cc089ce16f0835808e4
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-21 14:18:21 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters, idle
 from Config import config
 
 tgstore = Client(
+       ":storing:",
        api_id=config.API_ID,
        api_hash=config.API_HASH,
        bot_token=config.BOT_TOKEN,

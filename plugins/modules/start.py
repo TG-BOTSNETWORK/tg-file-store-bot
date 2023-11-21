f7bd521c40faeed9490513d9efb8e3864b61b3cf
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-21 16:26:41 +0530

    Update start.py

--- plugins/modules/start.py
+++ plugins/modules/start.py
@@ -1,6 +1,7 @@
 from pyrogram import filters, Client
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.errors.exceptions import UserNotParticipant
+from plugins import app 
 
 force_channel_id = -1002119954783
 

0e0103e7440ac7957dc955e83bb129857921d53c
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-23 13:34:39 +0530

    converted to sql

---
+++ plugins/database/premium.py
@@ -0,0 +1,14 @@
+from plugins.database import Connect
+
+def add_premium_user(user_id):
+    query = "INSERT INTO premium_users (user_id) VALUES (%s) ON CONFLICT DO NOTHING;"
+    Connect(query, (user_id,))
+
+def get_premium_users():
+    query = "SELECT COUNT(user_id) FROM premium_users;"
+    result = Connect(query, fetch=True)
+    return result[0][0] if result else 0
+
+def delete_premium_user(user_id):
+    query = "DELETE FROM premium_users WHERE user_id = %s;"
+    Connect(query, (user_id,))

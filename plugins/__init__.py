d6e1d9a47128fb87bd1714085e0eafe0ee02eb1c
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-23 17:55:04 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,11 +1,77 @@
-from pyrogram import Client, idle
+from aiohttp import web
+from pyrogram import Client, filters
+from pyrogram.enums import ParseMode
+import sys
+from datetime import datetime
 from Config import config
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 
-bot = Client(
-    ":santhu:",  
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-    plugins=dict(root="plugins.modules"),
+BOT_WORKERS = "4"
+
+LOG_FILE_NAME = "tgfilestoringbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
 )
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Bot",
+            api_hash=config.API_HASH,
+            api_id=config.APP_ID,
+            plugins={
+                "root": "plugins"
+            },
+            workers=config.BOT_WORKERS,
+            bot_token=config.BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
+
+        try:
+            db_channel = await self.get_chat(config.CHANNEL_ID)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
+            await test.delete()
+        except Exception as e:
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(
+                f"Make sure the bot is an admin in the DB Channel, and double-check the CHANNEL_ID Value. Current Value: {config.CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped.")
+            sys.exit()
+
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nt.me/TgBotsNetwork")
+        self.LOGGER(__name__).info(""" \n\n       
+ _____ ____   ____   ___ _____ ____    _   _ _____ _______        _____  ____  _  __
+|_   _/ ___| | __ ) / _ \_   _/ ___|  | \ | | ____|_   _\ \      / / _ \|  _ \| |/ /
+  | || |  _  |  _ \| | | || | \___ \  |  \| |  _|   | |  \ \ /\ / / | | | |_) | ' / 
+  | || |_| | | |_) | |_| || |  ___) | | |\  | |___  | |   \ V  V /| |_| |  _ <| . \ 
+  |_| \____| |____/ \___/ |_| |____/  |_| \_|_____| |_|    \_/\_/  \___/|_| \_\_|\_\ """)
+        self.username = usr_bot_me.username
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")

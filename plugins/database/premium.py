51b9ab61fcaf4d7d9a7d7ca51dc7936261d8a388
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-23 14:06:00 +0530

    Update premium.py

--- plugins/database/premium.py
+++ plugins/database/premium.py
@@ -1,14 +1,23 @@
 from plugins.database import Connect
 
+
+create_premium_users_table_query = """
+CREATE TABLE IF NOT EXISTS premium_users (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT UNIQUE NOT NULL
+);
+"""
+connect(create_premium_users)
+
 def add_premium_user(user_id):
     query = "INSERT INTO premium_users (user_id) VALUES (%s) ON CONFLICT DO NOTHING;"
-    Connect(query, (user_id,))
+    connect(query, (user_id,))
 
 def get_premium_users():
     query = "SELECT COUNT(user_id) FROM premium_users;"
-    result = Connect(query, fetch=True)
+    result = connect(query, fetch=True)
     return result[0][0] if result else 0
 
 def delete_premium_user(user_id):
     query = "DELETE FROM premium_users WHERE user_id = %s;"
-    Connect(query, (user_id,))
+    connect(query, (user_id,))

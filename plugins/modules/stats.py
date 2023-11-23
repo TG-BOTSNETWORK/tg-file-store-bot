3b4f83ad41c18219c5c408e45cd1ed9060b2843f
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-23 14:55:08 +0530

    Update stats.py

--- plugins/modules/stats.py
+++ plugins/modules/stats.py
@@ -1,7 +1,8 @@
 from pyrogram import Client, filters, __version__
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from plugins import bot 
-from plugins.database import add_user, add_chat, get_users, get_chats, get_uploaded_files_count, get_saved_files_count, get_deleted_files_count
+from plugins.database import add_user, add_chat, get_users, get_chats
+from plugins.database.save_files_sql import get_uploaded_files_count, get_saved_files_count, get_deleted_files_count
 from plugins.database.premium import add_premium_user, get_premium_users, delete_premium_user
 from Config import config
 

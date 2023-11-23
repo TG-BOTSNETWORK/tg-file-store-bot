3cd6c50ca149530b9d87d41d4a9ffd00da400d64
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-23 15:18:47 +0530

    Update __init__.py

--- plugins/database/__init__.py
+++ plugins/database/__init__.py
@@ -1,58 +1,47 @@
-import psycopg2
-from Config import config
+from plugins.database import Connect
 
-def Connect(query, values=None, fetch=False):
-    connection = psycopg2.connect(config.DATABASE_URL, sslmode='require')
-    cursor = connection.cursor()
-
-    try:
-        if values:
-            cursor.execute(query, values)
-        else:
-            cursor.execute(query)
-
-        connection.commit()
-
-        if fetch:
-            return cursor.fetchall()
-
-    finally:
-        cursor.close()
-        connection.close()
-
-create_users = """
-CREATE TABLE IF NOT EXISTS users (
+CREATE_TABLE_TOTAL_FILES = """
+CREATE TABLE IF NOT EXISTS uploaded_files (
     id SERIAL PRIMARY KEY,
-    user_id BIGINT UNIQUE NOT NULL
+    file_id TEXT NOT NULL,
+    user_id BIGINT NOT NULL,
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 """
 
-create_chats = """
-CREATE TABLE IF NOT EXISTS chats (
+CREATE_TABLE_SAVED_FILES = """
+CREATE TABLE IF NOT EXISTS saved_files (
     id SERIAL PRIMARY KEY,
-    chat_id BIGINT UNIQUE NOT NULL
+    file_id TEXT NOT NULL,
+    user_id BIGINT NOT NULL,
+    save_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 """
 
-Connect(create_users)
-Connect(create_chats)
+CREATE_TABLE_DELETED_FILES = """
+CREATE TABLE IF NOT EXISTS deleted_files (
+    id SERIAL PRIMARY KEY,
+    file_id TEXT NOT NULL,
+    user_id BIGINT NOT NULL,
+    delete_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+"""
 
-def add_user(user_id):
-    query = "INSERT INTO users (user_id) VALUES (%s) ON CONFLICT DO NOTHING RETURNING id;"
-    result = Connect(query, (user_id,), fetch=True)
-    return result[0][0] if result else None
+Connect(CREATE_TABLE_TOTAL_FILES)
+Connect(CREATE_TABLE_SAVED_FILES)
+Connect(CREATE_TABLE_DELETED_FILES)
 
-def add_chat(chat_id):
-    query = "INSERT INTO chats (chat_id) VALUES (%s) ON CONFLICT DO NOTHING RETURNING id;"
-    result = Connect(query, (chat_id,), fetch=True)
-    return result[0][0] if result else None
-
-def get_users():
-    query = "SELECT COUNT(user_id) FROM users;"
+def get_total_files_count():
+    query = "SELECT COUNT(*) FROM uploaded_files;"
     result = Connect(query, fetch=True)
     return result[0][0] if result else 0
 
-def get_chats():
-    query = "SELECT COUNT(chat_id) FROM chats;"
+def get_saved_files_count():
+    query = "SELECT COUNT(*) FROM saved_files;"
+    result = Connect(query, fetch=True)
+    return result[0][0] if result else 0
+
+def get_deleted_files_count():
+    query = "SELECT COUNT(*) FROM deleted_files;"
     result = Connect(query, fetch=True)
     return result[0][0] if result else 0

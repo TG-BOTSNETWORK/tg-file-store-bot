3d3ef6d1c8179a64acfba8e89d3a9ecacb6c15d2
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-23 15:07:20 +0530

    Update save_files_sql.py

--- plugins/database/save_files_sql.py
+++ plugins/database/save_files_sql.py
@@ -1,28 +1,35 @@
 from plugins.database import Connect
 
+create_uploaded_files_table = """
 CREATE TABLE uploaded_files (
     id SERIAL PRIMARY KEY,
     file_id TEXT NOT NULL,
     user_id BIGINT NOT NULL,
     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+"""
 
+create_saved_files_table = """
 CREATE TABLE saved_files (
     id SERIAL PRIMARY KEY,
     file_id TEXT NOT NULL,
     user_id BIGINT NOT NULL,
     save_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+"""
 
+create_deleted_files_table = """
 CREATE TABLE deleted_files (
     id SERIAL PRIMARY KEY,
     file_id TEXT NOT NULL,
     user_id BIGINT NOT NULL,
     delete_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-Connect(uploaded_files)
-Connect(saved_files)
-Connect(deleted_files)
+"""
+
+Connect(create_uploaded_files_table)
+Connect(create_saved_files_table)
+Connect(create_deleted_files_table)
 
 def get_uploaded_files_count():
     query = "SELECT COUNT(*) FROM uploaded_files;"

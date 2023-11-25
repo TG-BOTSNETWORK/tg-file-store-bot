b91049c4fdbd784002ffc3944bcee045cfa5242f
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-25 19:21:50 +0530

    Update info.py

--- plugins/modules/info.py
+++ plugins/modules/info.py
@@ -5,8 +5,6 @@ import os
 from plugins import bot as app
 from pyrogram.enums import ParseMode
 
-
-
 def get_user_info(user: User):
     user_info = (
         f"<b>Mention:</b> {user.mention}\n"
@@ -22,19 +20,27 @@ def get_user_info(user: User):
 @app.on_message(filters.command("info"))
 async def info_command(client: Client, message: Message):
     try:
+        msg = await message.edit_text("Searching user ID...")
         user_id = int(message.text.split(" ", 1)[1]) if len(message.text.split(" ", 1)) > 1 else None
         if user_id:
+            await msg.edit_text("Getting user info...")
             user = await client.get_users(user_id)
             user_info = get_user_info(user)
             profile_pic = await client.download_media(user.photo.big_file_id) if user.photo else None
+            await msg.edit_text("Uploading user info...")
             await message.reply_photo(photo=profile_pic, caption=user_info, parse_mode=ParseMode.HTML)
             if profile_pic:
                 os.remove(profile_pic)
         else:
+            await msg.edit_text("Getting user info...")
             user_info = get_user_info(message.from_user)
-            await message.reply_photo(photo=profike_pic, caption=user_info, parse_mode=ParseMode.HTML)
+            profile_pic = await client.download_media(message.from_user.photo.big_file_id) if message.from_user.photo else None
+            await msg.edit_text("Uploading user info...")
+            await message.reply_photo(photo=profile_pic, caption=user_info, parse_mode=ParseMode.HTML)
             if profile_pic:
                 os.remove(profile_pic)
+    except ValueError:
+        await message.reply_text("Invalid user ID. Please provide a valid numerical user ID.")
     except Exception as e:
         print(e)
-        await message.reply_text("Something went wrong.")
+        await message.reply_text(f"Something went wrong: {e}")

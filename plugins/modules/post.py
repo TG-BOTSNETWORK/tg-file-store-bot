d2c9956ddeacdd7ce8509e53aa2d616d5a603d23
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-11-25 17:31:02 +0530

    Update post.py

--- plugins/modules/post.py
+++ plugins/modules/post.py
@@ -59,7 +59,7 @@ async def channel_post(client: Client, message: Message):
         await post_message.edit_reply_markup(reply_markup)
 
 
-@bot.on_message(filters.channel & filters.incoming & filters.chat(config.CHANNEL_ID))
+@bot.on_message(filters.channel & filters.incoming & filters.user(config.OWNER_ID) & filters.chat(config.CHANNEL_ID))
 async def new_post(client: Client, message: Message):
     if DISABLE_CHANNEL_BUTTON:
         return
